e25cdf8a770ba16fe582beb090741c19fb5ecde4
Egor <ekurito@gmail.com>
2019-04-24 20:08:56 +0300

    add time measure for fib number

--- fib.py
+++ fib.py
@@ -1,14 +1,18 @@
+import time
+
+start1 = time.time()
 x = y = 1
-for i in range(20):
+for i in range(19):
     x, y = y, x + y
-    print(x)
+end1 = time.time()
+print('first: ',x,'time: ',end1 - start1)
 
-
+start2 = time.time()
 def f(x):
     if (x == 0) or (x == 1):
         return x
     t = f(x - 1) + f(x - 2)
     return (t)
+end2 = time.time()
 
-
-print(f(20))
+print('Second: ',f(20),'time: ',end2 - start2)
