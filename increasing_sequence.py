4df4db577dab76f4734a9bb82c7c14b5542c67c8
Egor <ekurito@gmail.com>
2019-05-12 21:36:01 +0300

    add  max_growing_subseq

--- increasing_sequence.py
+++ increasing_sequence.py
@@ -1,17 +1,15 @@
-a = []
-f = ("file.txt")
-for line in f:
-    a += list(map(int, line.split()))
+def max_growing_subseq(numb_arr):
+    n = len(numb_arr)
+    d = [1]*n
+    prev = [-1]*n
+    for i in range(n):
+        for j in range(i):
+            if numb_arr[i]>numb_arr[j] and d[i]<d[j]+1:
+                d[i] = d[j] + 1
+                prev[i] = j
+    print(d)
+    print(max(d), d.index(max(d)))
+    print(prev)
 
-print(a)
-
-n = len(a)
-d = [1]*n
-for i in range(n):
-    for j in range(i):
-        if a[i]>a[j] and d[i]<d[j]+1:
-            d[i] = d[j] + 1
-            print()
-
-print(d)
-l = d.index(max(d))
+test_arr = [1,3,2,3,5]
+max_growing_subseq(test_arr)
